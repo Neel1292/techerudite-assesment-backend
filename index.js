3ac325c885f54339c62c9004d9662680ae44b50c
Neel Prajapati <prajapatineel122002@gmail.com>
2025-04-03 19:25:13 +0530

    add CORS support, enhance UUID generation, and update email verification template

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const db = require("./config/connectDB")
+const cors = require("cors");
 require("dotenv").config();
 
 const authRoute = require('./routes/authRoute');
@@ -8,6 +9,7 @@ const authRoute = require('./routes/authRoute');
 const PORT = parseInt(process.env.PORT) || 5000;
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/auth', authRoute);

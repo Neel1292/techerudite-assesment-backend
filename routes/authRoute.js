8c11458f4ced10c7d25ab6879fa393b32087bc74
Neel Prajapati <prajapatineel122002@gmail.com>
2025-04-03 19:25:17 +0530

    changes made to the file

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const { handleCreateNewUser, handleLoginUser, handleVerifyUser } = require('../controller/authController');
+const { handleCreateNewUser, handleLoginUser, handleVerifyUser, handleVerifyToken } = require('../controller/authController');
 
 router.post('/register', handleCreateNewUser);
 router.post('/login', handleLoginUser);
+router.get('/verify', handleVerifyToken);
 router.patch('/verify', handleVerifyUser);
 
 module.exports = router
